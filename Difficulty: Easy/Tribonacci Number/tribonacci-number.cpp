a2f23395ca5e5467efb18029d61c03a7672f0a3a
DevKaran <64530394+kingdeepanshu@users.noreply.github.com>
2025-03-21 14:50:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Tribonacci Number/tribonacci-number.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    int solve(int n, vector<int> &dp){
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+        
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = solve(n-1, dp) + solve(n-2, dp) + solve(n-3, dp);
+    }
+    int nthTribonacci(int n) {
+        // code here
+        vector<int> dp(n+1, -1);
+        return solve(n, dp);
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        Solution obj;
+        int res = obj.nthTribonacci(n);
+
+        cout << res << endl;
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
