3dcb038a4e196507c7e5a0a6d4ca6c64cb71433c
DevKaran <64530394+kingdeepanshu@users.noreply.github.com>
2025-04-19 02:29:21 +0530

    Time: 1 ms (9.35%) | Memory: 13.1 MB (6.12%) - LeetSync

---
+++ SQL/198-house-robber/house-robber.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solve(vector<int> a, int i, int sum){
+        if(i >= a.size()){
+            return sum;
+        }
+        return max(solve(a, i+1, sum), solve(a, i+2, sum+a[i]));
+    }
+
+    int solvedp(vector<int> a, vector<int> &dp, int i){
+
+        if(i >= a.size()) return 0;
+
+        if(dp[i] != -1) return dp[i];
+
+        return dp[i] = max(a[i]+solvedp(a, dp, i+2), solvedp(a, dp, i+1));
+
+    }
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(), -1);
+
+        return solvedp(nums, dp, 0);
+    }
+};
