c0844789571e7aff5817828f0c4c20b8f60fafa2
DevKaran <64530394+kingdeepanshu@users.noreply.github.com>
2025-05-25 10:35:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Min Cost to Make Size 1/min-cost-to-make-size-1.cpp
+++ Difficulty: Easy/Min Cost to Make Size 1/min-cost-to-make-size-1.cpp
@@ -1,54 +1,11 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User function Template for C++
 class Solution {
   public:
 
     int cost(vector<int>& arr) {
         // code here
-        if(arr.size() == 1) return 0;
-        int min = arr[0];
-        for(auto i: arr){
-            if(min >= i){
-                min = i;
-            }
-        }
-        return min*(arr.size()-1);
+        sort(arr.begin(), arr.end());
+        int ans = arr[0] * (arr.size()-1);
+        return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int test_cases;
-    cin >> test_cases;
-    cin.ignore();
-    while (test_cases--) {
-        string input;
-        vector<int> arr;
-
-        // Read the array from input line
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        // Solution instance to invoke the function
-        Solution ob;
-        int result = ob.cost(arr);
-
-        cout << (result) << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
