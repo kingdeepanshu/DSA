bb68299e5974edc386aafbc9dd8598358547f808
DevKaran <64530394+kingdeepanshu@users.noreply.github.com>
2025-08-15 13:35:09 +0530

    Time: 17 ms (5.08%) | Memory: 24.7 MB (5.03%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -10,6 +10,25 @@ public:
             }
         }
     }
+
+    void bfs(unordered_map<int, list<int>> &al, vector<bool> &vis, int a){
+        queue<int> q;
+        q.push(a);
+        vis[a] = true;
+
+        while(!q.empty()){
+            int f = q.front();
+            q.pop();
+
+            for(int i: al[f]){
+                if(!vis[i]){
+                    q.push(i);
+                    vis[i] = true;
+                }
+            }
+        }
+    }
+
     int findCircleNum(vector<vector<int>>& isConnected) {
         unordered_map<int, list<int>> al;
         for(int i = 0; i < isConnected.size(); i++){
@@ -23,12 +42,15 @@ public:
 
         int v = isConnected.size();
 
-        int ans = v;
+        // int ans = v;
+        int ans = 0;
 
         vector<bool> vis(v, false);
         for(int i = 0; i < v; i++){
             if(!vis[i]){
-                dfs(al, vis, i, ans);
+                // dfs(al, vis, i, ans);
+                bfs(al, vis, i);
+                ans++;
             }
         }
 
